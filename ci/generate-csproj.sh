2e306de788d2c06208d0ea781c189c79af9f2a18
rhiltner <ryan.hiltner@gmail.com>
2026-04-06 22:02:04 -0500

    ci: add missing Unity module DLLs and reference them in csproj generator

--- ci/generate-csproj.sh
+++ ci/generate-csproj.sh
@@ -66,8 +66,20 @@ cat > "${OUT_DIR}/ASMLite.Editor.csproj" <<CSPROJ
       <HintPath>${UNITY_ENGINE_DLL}</HintPath>
       <Private>false</Private>
     </Reference>
+    <Reference Include="UnityEngine.CoreModule">
+      <HintPath>${REPO_ROOT}/${UNITY_PATH}/UnityEngine.CoreModule.dll</HintPath>
+      <Private>false</Private>
+    </Reference>
+    <Reference Include="UnityEngine.AnimationModule">
+      <HintPath>${REPO_ROOT}/${UNITY_PATH}/UnityEngine.AnimationModule.dll</HintPath>
+      <Private>false</Private>
+    </Reference>
     <Reference Include="UnityEditor">
       <HintPath>${UNITY_EDITOR_DLL}</HintPath>
+      <Private>false</Private>
+    </Reference>
+    <Reference Include="UnityEditor.CoreModule">
+      <HintPath>${REPO_ROOT}/${UNITY_PATH}/UnityEditor.CoreModule.dll</HintPath>
       <Private>false</Private>
     </Reference>
     <Reference Include="VRCSDKBase">
